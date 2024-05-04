15a145a80d2a3abeba524588d9dc0d064b4daaf5
Rodrigo Amarilla <amarillarodrigo14@gmail.com>
2024-05-04 19:23:24 -0300

    Creacion solucion parImpar

---
+++ assets/js/parImpar.js
@@ -0,0 +1,11 @@
+numero = parseInt(prompt('Ingrese un numero entero:'));
+
+if (numero % 1 === 0) {
+    if (numero % 2 === 0) {
+        console.log('El numero es Par.');
+    } else {
+        console.log('El numero es Impar.');
+    }
+} else {
+    console.log('No ha ingresado un numero entero, por favor intentelo otra vez.')
+}
