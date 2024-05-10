4f0afc34c5c83d7ddd19ac7676e078a9a61b2d13
Rodrigo Amarilla <amarillarodrigo14@gmail.com>
2024-05-10 11:04:57 -0300

    Correccion ejercicio nota promedio

--- Trabajo_Practico_2/assets/notaEstudiante.js
+++ Trabajo_Practico_2/assets/notaEstudiante.js
@@ -1,37 +1,44 @@
-let notaPrimero = parseInt(prompt('Ingrese una nota para el primer trimestre:'))
-
-let notaSegundo = parseInt(prompt('Ingrese una nota para el segundo trimestr:'))
-
-let notaTercero = parseInt(prompt('Ingrese una nota para el tercer trimestre:'))
-
-
-let promedio = (notaPrimero + notaSegundo + notaTercero) / 3;
-
-
-if (promedio > 0 && promedio <=10){
-    switch(promedio) {
-
-        case 1:
-        case 2:
-        case 3:
-            console.log('Nota Insuficiente');
+let nota1 = parseInt(prompt("Ingrese la nota del primer trimestre:"));
+let nota2 = parseInt(prompt("Ingrese la nota del segundo trimestre:"));
+let nota3 = parseInt(prompt("Ingrese la nota del tercer trimestre:"));
+let promedio = (nota1 + nota2 + nota3) / 3;
+if (
+    isNaN(nota1) ||
+    isNaN(nota2) ||
+    isNaN(nota3) ||
+    nota1 == ""  ||
+    nota2 == ""  ||
+    nota3 == ""
+    ) {
+    console.log("Ingrese solo numeros");
+    } else if (
+        nota1 < 0 ||
+        nota1 > 10 ||
+        nota2 < 0 ||
+        nota2 > 10 ||
+        nota3 < 0 ||
+        nota3 > 10
+      ) {
+        console.log('Ingrese solo numeros entre 0 y 10.')
+      } else {
+        switch (true) {
+            case promedio<=3 && promedio>=1:
+            console.log ("Nota Insuficiente")
+            break
+            case promedio == 4 || promedio == 5:
+            console.log ("Nota Regular")
+            break
+            case promedio == 6 || promedio == 7:
+            console.log ("Nota Buena")
+            break
+            case promedio == 8 || promedio == 9:
+            console.log ("Nota Muy Buena")
+            break
+            case promedio == 10:
+            console.log ("Nota Sobresaliente")
             break;
-        case 4:
-        case 5:
-            console.log('Nota Regular');
-            break;
-        case 6:
-        case 7:
-            console.log('Nota Buena');
-            break;
-        case 8:
-        case 9:
-            console.log('Nota Muy Buena');
-            break;
-        case 10:
-            console.log('Nota Sobresaliente');
-            break;
+            default:
+                console.log ("El promedio no pudo ser calculado, debido a los valores ingresados.")
+                break;
+        }
     }
-} else {
-    console.log('El promedio no pudo ser calculado porque no ha ingresado notas validas.');
-};
